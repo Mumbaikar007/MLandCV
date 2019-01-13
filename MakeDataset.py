4e17a4238b46b991a7a4bf1ab35680611cd3ed2e
Mumbaikar007 <brainteaser97@gmail.com>
2019-01-13 22:37:11 +0530

    Update MakeDataset.py

--- MakeDataset.py
+++ MakeDataset.py
@@ -9,9 +9,6 @@ im3 = im.copy()
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
 thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
-
-#################      Now finding Contours         ###################
-
 contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
 samples =  np.empty((0,100))
